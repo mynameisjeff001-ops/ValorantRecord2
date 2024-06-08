0ea31c996b6612a5160988627f5763c5a69dc97a
mynameisjeff001-ops <juan.na.makabayan@gmail.com>
2024-06-08 16:51:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+((
+  /** @type {string} */ streamUptimeString,
+  /** @type {string} */ streamStartDateString,
+  /** @type {string} */ urlEncodedGetMmrHistoryResponseJson,
+  /** @type {string} */ playerName,
+) => {
+
+  /* streamStartDateString will be a date string even if the channel is not currently live (the date will be the current
+     date). This may be a Nightbot bug. This is why streamUptimeString is needed to check whether the channel is live */
+  if (/\bnot live\b/i.test(streamUptimeString)) {
+    return `${playerName} is not live.`;
+  }
+
+  const streamStartDate = new Date(streamStartDateString);
+  if (Number.isNaN(streamStartDate.valueOf())) {
+    return `Failed to parse stream start date: ${streamStartDateString}`.slice(0, 400);
+  }
+
+  const getMmrHistoryResponseJson = decodeURIComponent(urlEncodedGetMmrHistoryResponseJson);
+  if (/^Error Connecting To Remote Server\b/i.test(getMmrHistoryResponseJson)) {
+    return getMmrHistoryResponseJson;
+  }
+
+  try {
+    /** @type {{
+      readonly data: ReadonlyArray<{
+        readonly mmr_change_to_last_game: number;
+        readonly date_raw: number;
+      }>;
+    }} */
+    const getMmrHistoryResponse = JSON.parse(getMmrHistoryResponseJson);
+
+    let winCountThisStream = 0;
+    let lossCountThisStream = 0;
+    let drawCountThisStream = 0;
+    
+    let latestMatchThisStream = 0;
+    let latestRawEloThisStream = null;
+    let earliestMatchThisStream = Number.POSITIVE_INFINITY;
+    let earliestRawEloThisStream = null;
+
+    for (const {date_raw: dateUnixS, mmr_change_to_last_game: mmrChange, elo: rawElo} of getMmrHistoryResponse.data) {
+      const date = new Date(dateUnixS * 1000);
+      if (date >= streamStartDate) {
+        if (mmrChange > 0) {
+          winCountThisStream++;
+        }
+        else if (mmrChange == 0) {
+          drawCountThisStream++;
+        }
+        else {
+          lossCountThisStream++;
+        }
+
+        if (latestMatchThisStream < date) {
+          latestMatchThisStream = date;
+          latestRawEloThisStream = rawElo;
+        }
+        if (earliestMatchThisStream > date) {
+          earliestMatchThisStream = date;
+          earliestRawEloThisStream = rawElo - mmrChange;
+        }
+      }
+    }
+    let fullStreamEloChange = latestRawEloThisStream - earliestRawEloThisStream;
+
+    return `${playerName} is ${fullStreamEloChange >= 0 ? 'up' : 'down'} ${fullStreamEloChange} RR this stream. Currently ${winCountThisStream}W - ${lossCountThisStream}L - ${drawCountThisStream}D. ${lastGameMmrChange >= 0 ? 'Won' : 'Lost'} ${Math.abs(lastGameMmrChange)} last game.`;
+  } catch (e) {
+    return `Failed to parse MMR history: ${e.message}: ${getMmrHistoryResponseJson}`.slice(0, 400);
+  }
+})
